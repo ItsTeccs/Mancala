719154ea7e7b4037fb2f816d9669b0479d243484
Tyler Eccles <eccles_tyler@yahoo.com>
2023-03-01 13:23:37 -0600

    basic movement
    adding basic movement mechanic and game logic handler

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include "mancala.h"
 
 class Mancala
 {
@@ -12,19 +11,76 @@ class Mancala
         this->bottomScore = 4;
     }
 
-    void Move()
+    void Move(int position)
     {
-        this->DisplayBoard(this->topScore, this->bottomScore, this->topRow, this->bottomRow);
+        /* 
+         * main gameplay handler function for taking turns
+         * handles game logic for each player as well as allocating points around the board
+         */
+         bool moveValid = false;
+         int numMoves;
+         do
+         {
+            if(player == 0 && (position >= 6 && position <= 11))
+            {
+                std::cout << "Invalid move entered. The top row player may only enter positions 0 - 5 to move.\n";
+            }
+            else if(player == 1 && (position >= 0 && position <= 5))
+            {
+                std::cout << "Invalid move entered. The bottom row player may only enter positions 6 - 11 to move.\n";
+            }
+            else
+            {
+                moveValid = true;
+            }
+         }while(!moveValid);
+
+         if(player == 0)
+         {
+            this->y = 0;
+            this->x = position;
+         }
+         else if (player == 1)
+         {
+            this->y = 1;
+            this->x = position - 6;
+         }
+
+
+        numMoves = board[this->y][this->x];
+        board[this->y][this->x] = 0;
+         for(int i = 0; i < numMoves)
+         {
+            if(this->x == 0 && this->y == 0)
+            {
+                this->y = 1;
+            }
+            else if(this->x == 5 && this->y == 1)
+            {
+                this->y = 0;
+            }
+
+            if(this->y == 0)
+            {
+            
+            }
+            else if (this->y == 1)
+            {
+
+            }
+         }
     }
 
     private:
+    int currentPlayer = 0 ; // 0 for top player, 1 for bottom player
     int x = 0;
     int y = 0;
     int topScore;
     int bottomScore;
     int topRow[6] = {14, 4, 4, 4, 4, 4};
     int bottomRow[6] = {14, 4, 4, 4, 4, 4};
-
+    int* board[2] = {topRow, bottomRow};
+    bool gameInProgress = true;
     void DisplayBoard(int topScore, int bottomScore, int topRow[], int bottomRow[])
     {
         std::string spacing = " ";
@@ -60,7 +116,7 @@ class Mancala
 
 int main(){
     Mancala gameBoard;
-    gameBoard.Move();
+    //gameBoard.Move();
 
     return 0;
 
