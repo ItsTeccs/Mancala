a98dd0acb16cf75e02b1bd130ff5f4d63b6876fa
ItsTeccs <eccles_tyler@yahoo.com>
2023-03-06 19:31:51 -0600

    movement
    move() function updated, board still buggy

--- main.cpp
+++ main.cpp
@@ -11,47 +11,84 @@ class Mancala
         this->bottomScore = 4;
     }
 
-    void Move(int position)
+    void Move()
     {
         /* 
          * main gameplay handler function for taking turns
          * handles game logic for each player as well as allocating points around the board
          */
-         bool moveValid = false;
-         int numMoves;
-         do
+        bool moveValid = false;
+        int numMoves;
+        int position;
+        if(this->player == 0)
+        {
+            std::cout << "Top row player input position 0 - 5 to move.";
+            std::cin >> position;
+        }
+        else
+        {
+            std::cout << "Top row player input position 0 - 5 to move.";
+            std::cin >> position;
+            // when player == 1
+        }
+
+        do
          {
-            if(player == 0 && (position >= 6 && position <= 11))
+            if(this->player == 0 && (position >= 6 && position <= 11))
             {
                 std::cout << "Invalid move entered. The top row player may only enter positions 0 - 5 to move.\n";
+                std::cin >> position;
+
             }
-            else if(player == 1 && (position >= 0 && position <= 5))
+            else if(this->player == 1 && (position >= 0 && position <= 5))
             {
                 std::cout << "Invalid move entered. The bottom row player may only enter positions 6 - 11 to move.\n";
+                std::cin >> position;
             }
             else
             {
                 moveValid = true;
             }
-         }while(!moveValid);
+        }while(!moveValid);
 
-         if(player == 0)
+        if(this->player == 0)
          {
             this->y = 0;
             this->x = position;
          }
-         else if (player == 1)
+        else if (player == 1)
          {
             this->y = 1;
             this->x = position - 6;
          }
 
 
-        numMoves = board[this->y][this->x];
-        board[this->y][this->x] = 0;
-         for(int i = 0; i < numMoves)
-         {
-            if(this->x == 0 && this->y == 0)
+        numMoves = this->board[this->y][this->x];
+        this->board[this->y][this->x] = 0;
+        if(this->x == 5 && this->y == 0)
+        {
+            this->y = 1;
+        }
+        else if(this->x == 0 && this->y == 1)
+        {
+            this->y = 0;
+        }
+
+        if(y == 0)
+        {
+            this->x++;
+        }
+        else
+        {
+            this->x--;
+        }
+
+
+        for(int i = 0; i < numMoves; i++)
+        {
+            this->board[this->y][this->x]++;
+            
+            if(this->x == 5 && this->y == 0)
             {
                 this->y = 1;
             }
@@ -60,19 +97,52 @@ class Mancala
                 this->y = 0;
             }
 
-            if(this->y == 0)
+            if(y == 0)
             {
-            
+                this->x++;
             }
-            else if (this->y == 1)
+            else
             {
+                this->x--;
+            }
+        }
+        this->player = ++this->player % 2;
+    }
 
-            }
-         }
+    void DisplayBoard()
+    {
+        std::string spacing = " ";
+        std::string middle_spacing(31, ' ');
+
+        std::cout 
+        << "[    ] "
+        << '[' << std::setw(2) << this->topRow[0] <<  ']' << spacing
+        << '[' << std::setw(2) << this->topRow[1] <<  ']' << spacing
+        << '[' << std::setw(2) << this->topRow[2] <<  ']' << spacing
+        << '[' << std::setw(2) << this->topRow[3] <<  ']' << spacing
+        << '[' << std::setw(2) << this->topRow[4] <<  ']' << spacing
+        << '[' << std::setw(2) << this->topRow[5] <<  ']' << spacing
+        << "[    ] "  << '\n';
+        
+        std::cout 
+        << '[' << std::setw(3) << this->topScore << ' ' << ']'
+        << middle_spacing
+        << '[' << std::setw(3) << this->bottomScore << ' ' << ']'
+        << '\n';
+
+        std::cout 
+        << "[    ] " 
+        << '[' << std::setw(2) << this->bottomRow[0] <<  ']' << spacing
+        << '[' << std::setw(2) << this->bottomRow[1] <<  ']' << spacing
+        << '[' << std::setw(2) << this->bottomRow[2] <<  ']' << spacing
+        << '[' << std::setw(2) << this->bottomRow[3] <<  ']' << spacing
+        << '[' << std::setw(2) << this->bottomRow[4] <<  ']' << spacing
+        << '[' << std::setw(2) << this->bottomRow[5] <<  ']' << spacing
+        << "[    ] "  << '\n';
     }
 
     private:
-    int currentPlayer = 0 ; // 0 for top player, 1 for bottom player
+    int player = 0 ; // 0 for top player, 1 for bottom player
     int x = 0;
     int y = 0;
     int topScore;
@@ -81,42 +151,13 @@ class Mancala
     int bottomRow[6] = {14, 4, 4, 4, 4, 4};
     int* board[2] = {topRow, bottomRow};
     bool gameInProgress = true;
-    void DisplayBoard(int topScore, int bottomScore, int topRow[], int bottomRow[])
-    {
-        std::string spacing = " ";
-        std::string middle_spacing(31, ' ');
-
-        std::cout 
-        << "[    ] "
-        << '[' << std::setw(2) << topRow[0] <<  ']' << spacing
-        << '[' << std::setw(2) << topRow[1] <<  ']' << spacing
-        << '[' << std::setw(2) << topRow[2] <<  ']' << spacing
-        << '[' << std::setw(2) << topRow[3] <<  ']' << spacing
-        << '[' << std::setw(2) << topRow[4] <<  ']' << spacing
-        << '[' << std::setw(2) << topRow[5] <<  ']' << spacing
-        << "[    ] "  << '\n';
-        
-        std::cout 
-        << '[' << std::setw(3) << topScore << ' ' << ']'
-        << middle_spacing
-        << '[' << std::setw(3) << bottomScore << ' ' << ']'
-        << '\n';
-
-        std::cout 
-        << "[    ] " 
-        << '[' << std::setw(2) << bottomRow[0] <<  ']' << spacing
-        << '[' << std::setw(2) << bottomRow[1] <<  ']' << spacing
-        << '[' << std::setw(2) << bottomRow[2] <<  ']' << spacing
-        << '[' << std::setw(2) << bottomRow[3] <<  ']' << spacing
-        << '[' << std::setw(2) << bottomRow[4] <<  ']' << spacing
-        << '[' << std::setw(2) << bottomRow[5] <<  ']' << spacing
-        << "[    ] "  << '\n';
-    }
 };
 
 int main(){
     Mancala gameBoard;
-    //gameBoard.Move();
+    gameBoard.DisplayBoard();
+    gameBoard.Move();
+    gameBoard.DisplayBoard();
 
     return 0;
 
